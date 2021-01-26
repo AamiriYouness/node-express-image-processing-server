babf5cb7948b44a9967c2b67d8f285013bc566a4
Youness Aamiri <youness.aamiri@pm.me>
2021-01-26 10:39:29 +0100

    Fixed test errors for second module

--- api/src/router.js
+++ api/src/router.js
@@ -1,35 +1,36 @@
-const { Router: router } = require('express');
+const { Router } = require('express');
 const multer = require('multer');
+const path = require('path');
 
-function filename(request, file, callback) {
+const router = Router();
+
+const filename = (request, file, callback) => {
   callback(null, file.originalname);
-}
+};
 
-function fileFilter(request, file, callback) {
-  if (file.mimetype != 'image/png') {
+const storage = multer.diskStorage({
+  destination: 'api/uploads/',
+  filename,
+});
+
+const fileFilter = (request, file, callback) => {
+  if (file.mimetype !== 'image/png') {
     request.fileValidationError = 'Wrong file type';
     callback(null, false, new Error('Wrong file type'));
   } else {
     callback(null, true);
   }
-}
-
-const storage = multer.diskStorage({
-  destination: 'api/uploads',
-  filename: filename,
-});
+};
 
 const upload = multer({
   fileFilter,
-  storage: storage,
+  storage,
 });
 
 router.post('/upload', upload.single('photo'), (request, response) => {
-  if (request.fileValidationError) {
-    return response.status(400).json({ error: request.fileValidationError });
-  } else {
-    return response.status(201).json({ success: true });
-  }
+  if (request.fileValidationError) return response.status(400).json({ error: request.fileValidationError });
+
+  return response.status(201).json({ success: true });
 });
 
 module.exports = router;
